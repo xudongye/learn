703e6d51f7f640871f007bbe41324dc802e977d6
yexudong <simonye@aliyun.com>
2019-04-17 15:41:56 +0800

    集成springfox-swagger-ui

---
+++ learn-configuration/learn-configuration-core/src/main/java/me/own/learn/configuration/service/swagger/WebAppConfig.java
@@ -0,0 +1,23 @@
+package me.own.learn.configuration.service.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author yexudong
+ * @date 2019/1/4 10:48
+ */
+@Configuration
+@EnableWebMvc
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
